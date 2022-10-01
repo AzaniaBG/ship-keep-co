c6d3659f14d2ebf7d1c459a2afedab02277d8d8e
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-10-01 12:25:14 -0700

    added hooks directory with custom input hook

---
+++ src/hooks/UseInputHook.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function() {
+
+    return (
+        <input
+            data-testid="input"
+            type="text"
+            placeholder="name"
+        />
+    );
+}
