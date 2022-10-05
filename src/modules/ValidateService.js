697652906be871e96e97308b7565c57cbdee31b8
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-10-05 07:30:30 -0700

    added validation input fn

--- src/modules/ValidateService.js
+++ src/modules/ValidateService.js
@@ -7,9 +7,36 @@ const ValidateService = {
     },
 
     isValidPassword(password) {
-        const validPassword = new RegExp(/(?=.*[A-Z])(?=.*[0-9])(?=.*[\.\-!@#$%^&*])/);
+        const validPassword = new RegExp(/(?=.*[A-Z])(?=.*[0-9])(?=.*[\.\-\\\/<>\|,;:\[\]}\_{"'?!@#$%^&*])/);
         const isValid = validPassword.test(password);
         return isValid;
+    },
+
+    isPasswordMatch(password, confirmPassword, setError) {
+        if(password === confirmPassword) {
+            setError(false);
+        } else {
+            setError(true);
+        };
+    },
+
+    validateInput(type, value, setError) {
+        if(type === "email") {
+            let validEmail = ValidateService.isValidEmail(value);
+            if(validEmail) {
+                setError(false);
+            } else {
+                setError(true);
+            };
+        };
+        if(type === "password") {
+            let validPassword = ValidateService.isValidPassword(value);
+            if(validPassword) {
+                setError(false);
+            } else {
+                setError(true);
+            };
+        };
     },
 };
 
